ad36e449004a948ff11c14c5cc370be86492f977
kovalevska olha <146865293+KovalevskaOlga@users.noreply.github.com>
2025-06-11 23:44:20 +0300

    Updates

---
+++ test.2de67bd9.js
@@ -0,0 +1,2 @@
+new Swiper(".swiper-container",{loop:!0,slidesPerView:1,spaceBetween:30,speed:800,effect:"fade",fadeEffect:{crossFade:!0},autoplay:{delay:1400,disableOnInteraction:!1},pagination:{el:".swiper-pagination",clickable:!0},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});
+//# sourceMappingURL=test.2de67bd9.js.map
